050174f7a0f9e148667e84cafc1de35f66cdb57b
Chris Fitkin <chrisfitkin@bemaverick.com>
2018-09-03 22:28:20 -0700

    Add coverage for Post user and comments resolvers

--- src/graphql/types/post.spec.js
+++ src/graphql/types/post.spec.js
@@ -1,8 +1,13 @@
 const { describe, test } = require('mocha');
-const { expect } = require('chai');
+const chai = require('chai');
+const spies = require('chai-spies');
 const { graphql } = require('graphql');
 const { makeExecutableSchema, addMockFunctionsToSchema, mockServer } = require('graphql-tools');
 const { typeDefs } = require('../schema');
+const { resolvers } = require('./post');
+
+chai.use(spies);
+const { expect } = chai;
 
 const testCases = [
   {
@@ -109,5 +114,68 @@ describe('GraphQL Post Schema', () => {
       if (expected) expect(result).to.deep.equal(expected);
       if (errors) expect(result).to.have.own.property('errors');
     });
+  });
+});
+
+describe('GraphQL Post Resolvers', () => {
+  test('Post.user', () => {
+    const post = {
+      id: '1',
+      body: 'Lorem',
+      title: 'Lorem',
+      user: '1',
+      comments: ['1', '1'],
+    };
+
+    // Mock the Dynamoose User Model
+    const mockUserModel = chai.spy.interface({
+      get: ({ id }) => ({ id, username: 'Lorem', email: 'Lorem' }),
+    });
+
+    const args = {};
+    const ctx = { User: mockUserModel };
+
+    const result = resolvers.Post.user(post, args, ctx);
+
+    expect(mockUserModel.get).to.be.called.once.with({ id: '1' });
+    expect(result).to.deep.equal({ id: '1', username: 'Lorem', email: 'Lorem' });
+  });
+  test('Post.comments', () => {
+    const post = {
+      id: '1',
+      body: 'Lorem',
+      title: 'Lorem',
+      user: '1',
+      comments: ['1', '1'],
+    };
+
+    // Mock the Dynamoose Comment Model
+    const mockCommentModel = chai.spy.interface({
+      scan: ({ post: { eq: id } }) => ({
+        exec: () => [
+          {
+            id,
+            post: '1',
+            user: '1',
+            body: 'Lorem',
+          },
+        ],
+      }),
+    });
+
+    const args = {};
+    const ctx = { Comment: mockCommentModel };
+
+    const result = resolvers.Post.comments(post, args, ctx);
+
+    expect(mockCommentModel.scan).to.be.called.once.with({ post: { eq: '1' } });
+    expect(result).to.deep.equal([
+      {
+        id: '1',
+        post: '1',
+        user: '1',
+        body: 'Lorem',
+      },
+    ]);
   });
 });
